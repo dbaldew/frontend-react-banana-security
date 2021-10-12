01460b1d8d1289c60a2c08fba6113bdbad338596
dbaldew <dbaldew@gmail.com>
2021-10-12 11:59:45 +0200

    AuthcontextProvider function / children

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,5 +1,17 @@
-
 import {createContext} from "react";
 
 //create AuthContext with createContext function / export / null
 export const AuthContext = createContext(null);
+
+//create AuthContext.Provider component/children
+function AuthContextProvider({children}){
+
+    return (
+        <AuthContext.Provider value={}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+//Export AuthContextProvider function
+export default AuthContextProvider;
